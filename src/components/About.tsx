6e68f34d64bb4c4d1775dc78342dd764a55d8a25
HafizaDeveloper <hafizamahnoor3005@gmail.com>
2024-11-10 22:50:51 +0500

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../app/styles/about.css"
+
+const About = () => {
+  return (
+    <div id='about' className='about-container'>
+      <h2 className='about-heading' data-aos="zoom-in-up">About Me</h2>
+      <p className='about-text' data-aos="zoom-in-up">
+      I am a dedicated and passionate developer with a growing skill set in both programming and web development. My journey in tech started with a strong foundation from my studies at <b>"M.Shafi Educational Complex"</b>. Beginning with languages like C, C++ and Java, I'm now expanding into modern web frameworks like Next.js and Tailwind CSS. I enjoy learning new technologies and applying them to real-world projects. Whether I’m working on web applications, exploring mobile development with Flutter, or enhancing my problem-solving skills, I’m always eager to learn and grow.
+      </p>
+    </div>
+  )
+}
+
+export default About
