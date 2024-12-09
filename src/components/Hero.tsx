4f7f7eedcb95dbf79f89e7b66f1cb34da5790e6b
Kinza-Developer <ubaidullahmayub203@gmail.com>
2024-12-09 20:26:19 +0500

    Fixed Deployment issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
     <div id="hero" className='hero-container'>
       <Navbar />
-      <div className='hero-content'>
+      <div className='hero-content min-'>
         <div className='hidden lg:block'></div>
         <div className='hero-text'>
         <div className='hero-main'>
-            <p data-aos="zoom-in-up">I'm</p>
+            <p data-aos="zoom-in-up">I am</p>
             <p data-aos="zoom-in-up">Hafiza</p>
             <p data-aos="zoom-in-up">Mahnoor</p>
           </div>
